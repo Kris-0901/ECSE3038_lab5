84f48b125c247732b17e3b243dcda625925cf1e4
Kris-0901 <tucker.kristof@gmail.com>
2025-03-16 10:36:02 -0500

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0); // setup for OLED display
 
 uint16_t messageIcon = 0x00EC; /*messageIcon (u8g2_font_streamline_all_t)*/
 
-void splashScreen();
+void splashScreen(); //OLED default startup screen function prototype
 
 void setup(){
 
@@ -27,8 +27,7 @@ void setup(){
 
   pinMode(BUILTIN_LED, OUTPUT);
 
-
-  while (WiFi.status() != WL_CONNECTED)
+  while (WiFi.status() != WL_CONNECTED) // wait for WiFi connection
   {
     delay(500);
     Serial.print(".");
@@ -41,53 +40,53 @@ void setup(){
   }
 
   Serial.print("Conected to the WiFi Network with IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP()); // print ip address 
 
 
   
 }
 
 void loop(){
-  if ( WiFi.status() == WL_CONNECTED) {
-    HTTPClient https;
+  if ( WiFi.status() == WL_CONNECTED) {// check if connected to Wifi
+    HTTPClient https; // declare http object
 
-    https.begin(ENDPOINT);
-    https.addHeader("api-key", API_KEY);
+    https.begin(ENDPOINT); // connect to endpoint/url
+    https.addHeader("api-key", API_KEY);// add api-key to header of get request
 
-    int responseCode=https.GET();
+    int responseCode=https.GET();// get the RestAPI status code
 
-    if (responseCode<=0){
+    if (responseCode<=0){// check for errors 
       Serial.print("An error occured with repsonse code:");
       Serial.println(responseCode);
       https.end();
       return;
     }
 
-    Serial.print("Status Code: ");
+    Serial.print("Status Code: ");// print stats code if no errors 
     Serial.println(responseCode);
 
-    String response= https.getString();
-    Serial.println(response);
-    https.end();
+    String response= https.getString(); // ge the body of the get request
+    Serial.println(response); // print body of get requuest
+    https.end();// end the https connection 
 
   }
 }
 
-void splashScreen()
+void splashScreen()//OLED default startup screen
 {
   const char* title = "Nokia";
   u8g2.firstPage();
   do {
-    u8g2.setFont(u8g2_font_streamline_all_t);
-    u8g2.drawGlyph(53, 42, messageIcon);
+    u8g2.setFont(u8g2_font_streamline_all_t);// set font 
+    u8g2.drawGlyph(53, 42, messageIcon);// display an icon 21x21 pixels in center of screen 
 
     int pos1 = 0;
     int pos2 = 128;
     int avaWidth = pos2 - pos1;
-    u8g2.setFont(u8g2_font_luBIS14_te);
-    int charWidth = u8g2.getStrWidth(title);
-    int centerStart = pos1 + (avaWidth - charWidth) / 2;
-    u8g2.drawStr(centerStart, 60, title);  //pass buffer // dummy data
+    u8g2.setFont(u8g2_font_luBIS14_te);// set font
+    int charWidth = u8g2.getStrWidth(title); // get width of string
+    int centerStart = pos1 + (avaWidth - charWidth) / 2; //print text in ceter of screen based on the width of text 
+    u8g2.drawStr(centerStart, 60, title); 
 
   } while ( u8g2.nextPage() );
 
